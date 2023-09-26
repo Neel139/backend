25bc7d67c7f70cab228c3609cbfe4e1044da8a95
Neel139 <np32978@gmail.com>
2023-09-26 23:01:34 +0530

    wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -14,6 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 try:
+    print("a")
     application = get_wsgi_application()
     app = application
 except Exception as e:
